6d298df65fb88d465616e8aab319d78e7018088d
chunl09 <815485731@qq.com>
2026-06-03 19:31:24 +0800

    fix name issue

--- layernorm/__init__.py
+++ layernorm/__init__.py
@@ -2,4 +2,4 @@ import torch
 from . import _C
 
 def layer_norm(input, gamma, beta, eps=1e-5):
-    return torch.ops.custom_layernorm._C.forward(input, gamma, beta, eps)
+    return torch.ops.layernorm_ops._C.forward(input, gamma, beta, eps)
--- layernorm/setup.py
+++ layernorm/setup.py
@@ -1,12 +1,9 @@
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
-# setup.py 示例
-from torch.utils.cpp_extension import CUDAExtension, BuildExtension
-
 ext_modules = [
     CUDAExtension(
-        name="custom_layernorm._C",  # 编译后的模块名
+        name="layernorm_ops._C",  # 编译后的模块名
         sources=[
             "csrc/layernorm.cpp",     # ← 从 csrc/ 读取
             "csrc/layernorm_cuda.cu",
@@ -19,8 +16,8 @@ ext_modules = [
 ]
 
 setup(
-    name="custom_layernorm",
+    name="layernorm_ops",
     ext_modules=ext_modules,
     cmdclass={"build_ext": BuildExtension},
-    packages=["custom_layernorm"],
+    packages=["layernorm_ops"],
 )
